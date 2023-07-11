d7910bff005e338bd2798530fee1a0fd8896134f
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2023-07-11 12:32:26 -0400

    logo and title changed

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>My Take</title>
+        <link rel="icon" href="/New_Project.png" />
       </Head>
 
       <main>
